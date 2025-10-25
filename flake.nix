e9ce43b5f670f999ea52132c1217f8d9805f08da
Vandy Carlos <vandycarlos@gmail.com>
2025-10-25 16:16:02 -0300

    add support for nix and direnv

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.rustup pkgs.nixfmt-classic ];
+        };
+        formatter = pkgs.nixfmt-classic;
+      });
+}
